cee7f3e36049f57c4920de6c437d07fd775ee019
Shreye2101 <shreye2101@gmail.com>
2025-12-24 10:00:13 +0530

    ASG table in AwS Services

---
+++ src/pages/AwsServices/ASGTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TableHead } from './components/ServiceTable';
+import { ASG_DATA } from './data/ServiceData';
+
+export default function ASGTable({ data }) {
+  const columns = ["Resource ID", "Resource Name", "Region", "Desired Capacity", "Min Size", "Max Size", "Status"];
+
+  return (
+    
+  );
+}
